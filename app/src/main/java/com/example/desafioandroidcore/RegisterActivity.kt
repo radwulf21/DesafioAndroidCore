f7a7c45c23a542b7f237e4d684a4f13c28d19b0d
Raul Andrade <raul.andrade21@gmail.com>
2020-11-09 00:20:39 -0300

    Tela Register

---
+++ app/src/main/java/com/example/desafioandroidcore/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.desafioandroidcore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        setSupportActionBar(toolbar_register)
+    }
+}
